a9af9c100b28d8a40722fd7d5da9de1f2dec0bc7
Mohsin Javed <mohsin9042002@gmail.com>
2025-09-15 17:44:59 +0500

    index

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,97 +1,105 @@
 export const projectsData = [
-    {
-        id: 1,
-        name: 'Procurement Advisor',
-        description: "Designed a smart procurement advisor platform that leverages analytics to evaluate suppliers, track compliance, and optimize purchasing decisions,enhancing efficiency and cost-effectiveness in supply chain operations.",
-        tools: ['Angular 16', "TypeScript", "MySQL" ,'Spring Boot','AWS S3'],
-        role: 'Frontend Developer',
-        code: '',
-        demo: '',
-    },
-    {
-        id: 2,
-        name: 'LoopHr',
-        description: 'I contributed to the development of Loop HR, a human resources application. The application is designed to streamline HR processes,including employee data management, attendance tracking, payroll handling, and performance monitoring. ',
-        tools: ['Flutter', 'Dart','MobX','Spring Boot','AWS S3'],
-        role: 'Frontend Developer',
-        code: '',
-        demo: '',
-    },
-    {
-        id: 3,
-        name: 'LEARNING MANAGEMENT SYSTEM (FRAPPE LMS)',
-        description: ' Developed a web-based platform for managing online courses, user roles, assignments, and progress tracking. Focused on a user-friendly interface and efficient content delivery using modern web technologies. ',
-        tools: ['Vue Js', 'Bootstrap', 'SCSS',  'MongoDB','Python'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-    },
   {
     id: 1,
-    name: 'PRIME MART (E-COMMERCE WEBSITE)',
-    description: "Developed a full-stack web application using PHP with HTML as the frontend. User-friendly interface that allows users to easily search products and order. Integrated PayPal payment gateway. Admin can upload, delete, and update products.",
-    tools: ['PHP', 'HTML', 'CSS', 'JavaScript', 'PayPal API'],
-    code: '',
-    demo: '',
-    role: 'Full Stack Developer',
-},
-{
+    name: "Procurement Advisor",
+    description:
+      "Designed a smart procurement advisor platform that leverages analytics to evaluate suppliers, track compliance, and optimize purchasing decisions,enhancing efficiency and cost-effectiveness in supply chain operations.",
+    tools: ["Angular 16", "TypeScript", "MySQL", "Spring Boot", "AWS S3"],
+    role: "Frontend Developer",
+    code: "",
+    demo: "",
+  },
+  {
     id: 2,
-    name: 'MERCHANDISE MANAGEMENT SYSTEM (WINDOWS APPLICATION)',
-    description: "Developed a Windows application using ReactJS with C#. User-friendly interface that allows users to easily manage stock. Admin can upload, delete, and update products. Features include stock availability, total cost, and sale price tracking.",
-    tools: ['ReactJS', 'C#', 'SQL Server'],
-    code: '',
-    demo: '',
-    role: 'Full Stack Developer',
-},
-{
+    name: "LoopHr",
+    description:
+      "I contributed to the development of Loop HR, a human resources application. The application is designed to streamline HR processes,including employee data management, attendance tracking, payroll handling, and performance monitoring. ",
+    tools: ["Flutter", "Dart", "MobX", "Spring Boot", "AWS S3"],
+    role: "Frontend Developer",
+    code: "",
+    demo: "",
+  },
+  {
     id: 3,
-    name: 'RESOURCIFY (WEB APPLICATION)',
-    description: "Designed and developed a web system to manage books, user borrowing, returns, and inventory tracking. Focused on streamlining library operations with features like search, user roles, and real-time availability using modern web technologies.",
-    tools: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'MongoDB'],
-    code: '',
-    demo: '',
-    role: 'Full Stack Developer',
-},
-{
+    name: "LEARNING MANAGEMENT SYSTEM (FRAPPE LMS)",
+    description:
+      " Developed a web-based platform for managing online courses, user roles, assignments, and progress tracking. Focused on a user-friendly interface and efficient content delivery using modern web technologies. ",
+    tools: ["Vue Js", "Bootstrap", "SCSS", "MongoDB", "Python"],
+    code: "",
+    role: "Full Stack Developer",
+    demo: "",
+  },
+  {
     id: 4,
-    name: 'KFC (CLONE)',
-    description: "Developed a Flutter-based mobile application replicating the KFC ordering experience. Features include product listings, cart management, order placement, and a responsive UI optimized for smooth user interaction.",
-    tools: ['Flutter', 'Dart', 'Firebase'],
-    code: '',
-    demo: '',
-    role: 'Mobile App Developer',
-},
-{
+    name: "PRIME MART (E-COMMERCE WEBSITE)",
+    description:
+      "Developed a full-stack web application using PHP with HTML as the frontend. User-friendly interface that allows users to easily search products and order. Integrated PayPal payment gateway. Admin can upload, delete, and update products.",
+    tools: ["PHP", "HTML", "CSS", "JavaScript", "PayPal API"],
+    code: "",
+    demo: "",
+    role: "Full Stack Developer",
+  },
+  {
     id: 5,
-    name: 'CHEEZIOUS (CLONE)',
-    description: "Developed a full-stack application clone using C# web APIs with Flutter as the frontend. User-friendly interface that allows users to easily search food and order. Admin can upload, delete, and update products.",
-    tools: ['Flutter', 'Dart', 'C#', 'ASP.NET Web API'],
-    code: '',
-    demo: '',
-    role: 'Full Stack Developer',
-},
-{
+    name: "MERCHANDISE MANAGEMENT SYSTEM (WINDOWS APPLICATION)",
+    description:
+      "Developed a Windows application using ReactJS with C#. User-friendly interface that allows users to easily manage stock. Admin can upload, delete, and update products. Features include stock availability, total cost, and sale price tracking.",
+    tools: ["ReactJS", "C#", "SQL Server"],
+    code: "",
+    demo: "",
+    role: "Full Stack Developer",
+  },
+  {
     id: 6,
-    name: 'SECURITY SAFE ZONE',
-    description: "Built a mobile and web app for personal safety, enabling real-time alerts, GPS-based safe zones, and quick emergency access using Flutter.",
-    tools: ['Flutter', 'Dart', 'Google Maps API', 'Firebase'],
-    code: '',
-    demo: '',
-    role: 'Mobile App Developer',
-},
-{
+    name: "RESOURCIFY (WEB APPLICATION)",
+    description:
+      "Designed and developed a web system to manage books, user borrowing, returns, and inventory tracking. Focused on streamlining library operations with features like search, user roles, and real-time availability using modern web technologies.",
+    tools: ["HTML", "CSS", "JavaScript", "Node.js", "MongoDB"],
+    code: "",
+    demo: "",
+    role: "Full Stack Developer",
+  },
+  {
     id: 7,
-    name: 'DOCTOR APPOINTMENT SYSTEM',
-    description: "Developed a cross-platform doctor appointment system that streamlines patient-doctor interactions by enabling users to search for specialists, view availability, and book appointments in real time. Integrated features include secure user authentication, appointment reminders, digital prescriptions, and medical history tracking.",
-    tools: ['Flutter', 'Dart', 'Firebase', 'Node.js'],
-    code: '',
-    demo: '',
-    role: 'Full Stack Developer',
-}
-
+    name: "KFC (CLONE)",
+    description:
+      "Developed a Flutter-based mobile application replicating the KFC ordering experience. Features include product listings, cart management, order placement, and a responsive UI optimized for smooth user interaction.",
+    tools: ["Flutter", "Dart", "Firebase"],
+    code: "",
+    demo: "",
+    role: "Mobile App Developer",
+  },
+  {
+    id: 8,
+    name: "CHEEZIOUS (CLONE)",
+    description:
+      "Developed a full-stack application clone using C# web APIs with Flutter as the frontend. User-friendly interface that allows users to easily search food and order. Admin can upload, delete, and update products.",
+    tools: ["Flutter", "Dart", "C#", "ASP.NET Web API"],
+    code: "",
+    demo: "",
+    role: "Full Stack Developer",
+  },
+  {
+    id: 9,
+    name: "SECURITY SAFE ZONE",
+    description:
+      "Built a mobile and web app for personal safety, enabling real-time alerts, GPS-based safe zones, and quick emergency access using Flutter.",
+    tools: ["Flutter", "Dart", "Google Maps API", "Firebase"],
+    code: "",
+    demo: "",
+    role: "Mobile App Developer",
+  },
+  {
+    id: 10,
+    name: "DOCTOR APPOINTMENT SYSTEM",
+    description:
+      "Developed a cross-platform doctor appointment system that streamlines patient-doctor interactions by enabling users to search for specialists, view availability, and book appointments in real time. Integrated features include secure user authentication, appointment reminders, digital prescriptions, and medical history tracking.",
+    tools: ["Flutter", "Dart", "Firebase", "Node.js"],
+    code: "",
+    demo: "",
+    role: "Full Stack Developer",
+  },
 ];
-
 
 // Do not remove any property.
 // Leave it blank instead as shown below
